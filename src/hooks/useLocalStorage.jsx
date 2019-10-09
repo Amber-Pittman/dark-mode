748109a7a648c2be81517b75c4ebc03cd5fdcfaf
Amber-Pittman <amber@amberpittman.com>
2019-10-08 21:22:54 -0400

    Made a note to self

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -8,7 +8,7 @@ export function useLocalStorage(key, initialValue) {
 
 
     // setter function
-    const setValue = value => {
+    const setValue = value => { // this is a wrapper around the setSomething setter
         setSomething(value); // saves state
         window.localStorage.getItem(key, JSON.stringify(value)); // JSON parses into a string
     }
