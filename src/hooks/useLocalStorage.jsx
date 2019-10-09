a401077071f001976cab410e8c39b7ac4eb4ccf7
Amber-Pittman <amber@amberpittman.com>
2019-10-08 21:02:01 -0400

    Set state and created setter function

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+    const [something, /*setSomething*/] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue; 
+    });
+
+
+    // setter function
+    const setValue = value => {
+        setStoredValue(value); // saves state
+        window.localStorage.getItem(key, JSON.stringify(value)); // JSON parses into a string
+    }
+
+    return [something];
+}
