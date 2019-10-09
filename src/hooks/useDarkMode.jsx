de4ad6b49d6a51be64dfd1e9fedaa20cacca8cf1
Amber-Pittman <amber@amberpittman.com>
2019-10-08 21:16:09 -0400

    Built Dark Mode function

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
 export function useDarkMode() {
-    
+    const [dark, setDark] = useLocalStorage("dark"); // "dark" is a key
+    useEffect(() => { // manipulate the DOM with useEffect
+        
+        // Is it dark?
+        dark ? document.body.classList.add("dark-mode") : // if true, adds the class to the body element
+            document.body.classList.remove("dark-mode") // if false, removes from body element
+    }, [dark])
+
+    return [dark, setDark]
 }
